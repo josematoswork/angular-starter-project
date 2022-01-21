98b58456697e0977f59a37c4ee6f4041a33a002e
José Matos <developer@josematos.work>
2022-01-21 16:15:24 +0000

    Base structure created

---
+++ src/app/@core/core.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HttpClientModule],
+  providers: [],
+  exports: []
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './@core/core.module';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './@shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, SharedModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
